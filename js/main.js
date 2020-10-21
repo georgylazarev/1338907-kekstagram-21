7ec6c151caa10aa9951b02d6094132e7e12eb06a
Георгий Лазарев <geolaz@gmail.com>
2020-10-21 16:14:51 +0300

    Вызов загрузки данных

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 'use strict';
 
-// Вешаем на все превью обработчик событий
-const allThumbnails = document.querySelectorAll(`.picture`);
-allThumbnails.forEach((thumbnail) => {
-  thumbnail.addEventListener(`click`, function (evt) {
-    evt.preventDefault();
-    window.preview.showBigPhoto(thumbnail);
+const onError = function (message) {
+  console.error(message);
+};
+
+const onSuccess = function (data) {
+  let listOfPhotos = document.querySelector(`.pictures`);
+  data.forEach((item, i) => {
+    listOfPhotos.appendChild(window.onLoadData.displayPicture(item, i));
   });
-});
+  window.allPhotos = {
+    data
+  };
+  // Вешаем на все превью обработчик событий
+  const allThumbnails = document.querySelectorAll(`.picture`);
+  allThumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener(`click`, function (evt) {
+      evt.preventDefault();
+      window.preview.showBigPhoto(thumbnail);
+    });
+  });
+};
+
+window.load(`https://21.javascript.pages.academy/kekstagram/data`, onSuccess, onError);
