0c8516f6012af2c02387ad221c6b89ea001147fd
Георгий Лазарев <geolaz@gmail.com>
2020-10-21 16:14:32 +0300

    Добавил код загрузки данных с сервера

---
+++ js/load.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const MAX_REQUEST_TIMEOUT = 3000;
+
+(function () {
+  window.load = function (url, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = `json`;
+
+    xhr.addEventListener(`load`, function () {
+      let error;
+      switch (xhr.status) {
+        case 200:
+          onSuccess(xhr.response);
+          break;
+
+        case 400:
+          error = `Неверный запрос`;
+          break;
+        case 401:
+          error = `Пользователь не авторизован`;
+          break;
+        case 404:
+          error = `Ничего не найдено`;
+          break;
+
+        default:
+          error = `Cтатус ответа: : ` + xhr.status + ` ` + xhr.statusText;
+      }
+
+      if (error) {
+        onError(error);
+      }
+    });
+
+    xhr.addEventListener(`error`, function () {
+      onError(`Произошла ошибка соединения`);
+    });
+
+    xhr.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+    });
+
+    xhr.timeout = MAX_REQUEST_TIMEOUT;
+
+    xhr.open(`GET`, url);
+    xhr.send();
+  };
+})();
