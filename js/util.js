639b9c2bf01762b1a655e800d24b07ade2c8f340
Георгий Лазарев <geolaz@gmail.com>
2020-10-30 22:26:56 +0300

    добавил функцию перемешивания массива

--- js/util.js
+++ js/util.js
@@ -25,6 +25,20 @@
         }
       };
       document.addEventListener(`keydown`, closeMessageByEsc);
+    },
+    shuffle(array) {
+      let currentIndex = array.length;
+      let temporaryValue;
+      let randomIndex;
+
+      while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
     }
   };
 })();
